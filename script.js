a775045e4506bbc4e288fbb61caae9f61fabe327
AdamFriedland5003 <51975257+AdamFriedland5003@users.noreply.github.com>
2019-06-19 09:47:56 -0700

    Added script (javascript)
    Script for index html

---
+++ script.js
@@ -0,0 +1,25 @@
+#art {
+    display: table;
+    border-spacing: 1px;
+    background-color: black;
+    border: 3px solid black;
+    
+}
+
+.row {
+  display: table-row;
+}
+
+.pixel {
+  display: table-cell;
+  background-color: white;
+  width: 20px;
+  height: 20px;
+}
+
+.pen {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 1px solid black;
+}
